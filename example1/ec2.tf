89eadddfa35d63a402440bbf8e2e22a6e66505ee
Balaji A <abj.balaji@gmail.com>
2022-05-26 10:17:36 +0530

    Updating

---
+++ example1/ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "sample" {
+  ami                    = "ami-0bb6af715826253bf"
+  instance_type          = "t3.micro"
+
+}
+
+output "public_ip" {
+  value = aws_instance.sample.public_ip
+}
