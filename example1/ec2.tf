6699ff0ee15bd853ee1f48ebec592d284a8ca408
Balaji A <abj.balaji@gmail.com>
2022-05-26 10:27:34 +0530

    Updating

--- example1/ec2.tf
+++ example1/ec2.tf
@@ -11,3 +11,27 @@ resource "aws_instance" "sample" {
 output "public_ip" {
   value = aws_instance.sample.public_ip
 }
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
